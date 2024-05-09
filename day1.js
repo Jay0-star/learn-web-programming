a3fe09e78985dd6262c1e999d3941ce1439934b3
Jay0-star <81146577+Jay0-star@users.noreply.github.com>
2024-05-09 06:30:42 +0000

    const let and var use

---
+++ day1.js
@@ -0,0 +1,7 @@
+const accountId = 12345
+let accountEmail = "yadavjay0988@gmail.com"
+var acountPassword = 2580
+accountCity = "sanhaitha"
+// accountId = 7
+console.log(accountId);
+console.table([accountId,accountEmail,acountPassword,accountCity])
